a688bf9bcae2cf864bdbf23bf0d8f3d4ebda64ac
shashwatsrivastava <shashrulz@gmail.com>
2016-03-15 15:50:27 -0400

    Changed database settings

--- RangingServer/settings.py
+++ RangingServer/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'RangingServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Internationalization
