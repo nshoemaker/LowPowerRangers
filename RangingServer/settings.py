e37cd28d48f67e5173ac35efbd2853af3cba2f68
shashwatsrivastava <shashrulz@gmail.com>
2016-03-15 15:38:49 -0400

    Made changes to settings

--- RangingServer/settings.py
+++ RangingServer/settings.py
@@ -113,3 +113,9 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
