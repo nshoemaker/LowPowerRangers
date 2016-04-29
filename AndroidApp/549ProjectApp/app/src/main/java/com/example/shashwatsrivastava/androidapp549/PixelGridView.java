c434fd50409904377a099e9290c2f0832c4a9cce
shashwatsrivastava <shashrulz@gmail.com>
2016-04-29 11:48:32 -0400

    Can now zoom and pan but the Tags dont move

--- AndroidApp/549ProjectApp/app/src/main/java/com/example/shashwatsrivastava/androidapp549/PixelGridView.java
+++ AndroidApp/549ProjectApp/app/src/main/java/com/example/shashwatsrivastava/androidapp549/PixelGridView.java
@@ -9,6 +9,8 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.widget.RelativeLayout;
 
+import java.util.zip.DeflaterInputStream;
+
 /**
  * Created by shashwatsrivastava on 4/21/16.
  */
@@ -21,6 +23,28 @@ public class PixelGridView extends RelativeLayout {
     // Will be used to take care of scaling
     private ScaleGestureDetector mScaleDetector;
     private float mScaleFactor = 1.f;
+
+    //These constants specify the mode that we're in
+    private static int NONE = 0;
+    private static int DRAG = 1;
+    private static int ZOOM = 2;
+
+    private int mode;
+
+    //These two variables keep track of the X and Y coordinate of the finger when it first
+    //touches the screen
+    private float startX = 0f;
+    private float startY = 0f;
+
+    //These two variables keep track of the amount we need to translate the canvas along the X
+    //and the Y coordinate
+    private float translateX = 0f;
+    private float translateY = 0f;
+
+    //These two variables keep track of the amount we translated the X and Y coordinates, the last time we
+    //panned.
+    private float previousTranslateX = 0f;
+    private float previousTranslateY = 0f;
 
     public PixelGridView(Context context) {
         this(context, null);
@@ -80,8 +104,35 @@ public class PixelGridView extends RelativeLayout {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        canvas.save();
-//        canvas.scale(mScaleFactor, mScaleFactor);
+        canvas.save();
+        canvas.scale(mScaleFactor, mScaleFactor);
+        float displayWidth = DisplayUtilities.getDpWidth();
+        float displayHeight = DisplayUtilities.getDpHeight();
+
+        //If translateX times -1 is lesser than zero, let's set it to zero. This takes care of the left bound
+        if((translateX * -1) < 0) {
+            translateX = 0;
+        }
+        //This is where we take care of the right bound. We compare translateX times -1 to (scaleFactor - 1) * displayWidth.
+        //If translateX is greater than that value, then we know that we've gone over the bound. So we set the value of
+        //translateX to (1 - scaleFactor) times the display width. Notice that the terms are interchanged; it's the same
+        //as doing -1 * (scaleFactor - 1) * displayWidth
+        else if((translateX * -1) > (mScaleFactor - 1) * displayWidth) {
+            translateX = (1 - mScaleFactor) * displayWidth;
+        }
+
+        if(translateY * -1 < 0) {
+            translateY = 0;
+        }
+
+        //We do the exact same thing for the bottom bound, except in this case we use the height of the display
+        else if((translateY * -1) > (mScaleFactor - 1) * displayHeight) {
+            translateY = (1 - mScaleFactor) * displayHeight;
+        }
+
+        //We need to divide by the scale factor here, otherwise we end up with excessive panning based on our zoom level
+        //because the translation amount also gets scaled according to how much we've zoomed into the canvas.
+        canvas.translate(translateX / mScaleFactor, translateY / mScaleFactor);
 
         canvas.drawColor(Color.BLACK);
 
@@ -99,13 +150,76 @@ public class PixelGridView extends RelativeLayout {
         for (int i = 1; i < numRows; i++) {
             canvas.drawLine(0, i * cellHeight, width, i * cellHeight, redPaint);
         }
-        //canvas.restore();
+        canvas.restore();
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        // Let the ScaleGestureDetector inspect all events.
-        mScaleDetector.onTouchEvent(ev);
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean dragged = true;
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+            case MotionEvent.ACTION_DOWN:
+                mode = DRAG;
+
+                //We assign the current X and Y coordinate of the finger to startX and startY minus the previously translated
+                //amount for each coordinates This works even when we are translating the first time because the initial
+                //values for these two variables is zero.
+                startX = event.getX() - previousTranslateX;
+                startY = event.getY() - previousTranslateY;
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                translateX = event.getX() - startX;
+                translateY = event.getY() - startY;
+
+                //We cannot use startX and startY directly because we have adjusted their values using the previous translation values.
+                //This is why we need to add those values to startX and startY so that we can get the actual coordinates of the finger.
+                double distance = Math.sqrt(Math.pow(event.getX() - (startX + previousTranslateX), 2) +
+                                Math.pow(event.getY() - (startY + previousTranslateY), 2)
+                );
+
+                if(distance > 0) {
+                    dragged = true;
+                }
+
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN:
+                mode = ZOOM;
+                break;
+
+            case MotionEvent.ACTION_UP:
+                mode = NONE;
+                dragged = false;
+
+                //All fingers went up, so let's save the value of translateX and translateY into previousTranslateX and
+                //previousTranslate
+                previousTranslateX = translateX;
+                previousTranslateY = translateY;
+                break;
+
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = DRAG;
+
+                //This is not strictly necessary; we save the value of translateX and translateY into previousTranslateX
+                //and previousTranslateY when the second finger goes up
+                previousTranslateX = translateX;
+                previousTranslateY = translateY;
+                break;
+        }
+
+        mScaleDetector.onTouchEvent(event);
+
+        //We redraw the canvas only in the following cases:
+        //
+        // o The mode is ZOOM
+        //        OR
+        // o The mode is DRAG and the scale factor is not equal to 1 (meaning we have zoomed) and dragged is
+        //   set to true (meaning the finger has actually moved)
+        if ((mode == DRAG && mScaleFactor != 1f && dragged) || mode == ZOOM) {
+            invalidate();
+        }
+
         return true;
     }
 
@@ -115,7 +229,7 @@ public class PixelGridView extends RelativeLayout {
             mScaleFactor *= detector.getScaleFactor();
 
             // Don't let the object get too small or too large.
-            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+            mScaleFactor = Math.max(1f, Math.min(mScaleFactor, 5.0f));
 
             invalidate();
             return true;
